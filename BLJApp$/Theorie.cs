009f053f3103c84e9d239f8b327ea8c4e5de9e1b
RubeldiRubelda <benjamin.kunz.ch@gmail.com>
2025-09-29 08:07:22 +0200

    first commit

---
+++ BLJApp$/Theorie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace progra1m
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int[] a = new int[5]; 
+a[0] = 1;
+a[1] = 5;
+a[2] = 9;
+a[3] = 13;
+a[4] = 17;
+
+Console.Write(a[2]); // schreibt 9 in die Konsole
+
+        }
+    }
+}
