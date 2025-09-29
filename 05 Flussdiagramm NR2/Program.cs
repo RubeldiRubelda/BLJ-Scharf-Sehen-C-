132f6f1259a02d9085c3cccb869595b46780c040
RubeldiRubelda <benjamin.kunz.ch@gmail.com>
2025-09-29 13:41:06 +0200

    ARBEITSZEITBETRUG ENDGEGNER (Jetzt haben die Programme ein Ladebalken ABSOLUTES FARBKINO)

---
+++ 05 Flussdiagramm NR2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int makingKilometers;
+
+
+
+            Console.WriteLine("Wie viele Kilometer möchtest du rennen?");
+            string input = Console.ReadLine();
+
+
+
+            if (int.TryParse(input, out makingKilometers) == true)
+            {
+                if (makingKilometers < 1 || makingKilometers > 42)
+                {
+                    Console.WriteLine("Das schaffst du nie! ");//42.195 km ist ein Marathon sagt Google (Crazy)
+                }
+                else
+                {
+
+                    Console.WriteLine("Berechne anzahl Bahnrunden... " + makingKilometers);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Eingabefehler. Bitte geben Sie eine Zahl ein.");
+            }
+        }
+    }
+}
---
+++ Spinner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        ShowSpinner(30000); // 3000 ms = 3 Sekunden
+
+        Console.WriteLine("Nächste Aufgabe geht hier weiter...");
+        // ab hier dein weiterer Code
+    }
+
+    static void ShowSpinner(int durationMs)
+    {
+        char[] spinner = { '-', '/', '|', '\\' };
+        int counter = 0;
+
+        DateTime endTime = DateTime.Now.AddMilliseconds(durationMs);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(spinner[counter % spinner.Length]);
+            Thread.Sleep(100); // Geschwindigkeit einstellen
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            counter++;
+        }
+
+        // nach der Animation noch ein Leerzeichen, damit der Spinner "verschwindet"
+        Console.Write(" ");
+        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+    }
+}
